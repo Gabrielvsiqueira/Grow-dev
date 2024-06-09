5bef572ca85cfdd3530b051d312c5c9e4b86f0e1
Gabrielvsiqueira <gabrielvitorsiqueira53@gmail.com>
2024-06-09 20:53:49 -0300

    usando a prop key em listas

--- curso-react/Section-3/avancado/src/components/ListRender.js
+++ curso-react/Section-3/avancado/src/components/ListRender.js
@@ -1,12 +1,24 @@
 import {useState} from "react"
 
 const ListRender = () => {
-    const list = useState(["Gabriel", "Paulo", "Bruno"]);
+    const [list] = useState(["Gabriel", "Paulo", "Bruno", "Maria"]);
+
+    const [users] = useState([
+        {id: 1, name: "Joao", age: 31},
+        {id: 7463721, name: "Pedro", age: 21},
+        {id: 233215, name: "Joaquim", age: 44},
+    ])
+
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((users) => (
+                  <li key={users.id}> {users.name} - {users.age}</li>  
                 ))}
             </ul>
         </div>
